71047c28666b5c081e1809717c32adf3ee6d692c
olga <bachisheo@gmail.com>
2021-11-15 18:59:09 +0700

    added cluster class

---
+++ src/Objects/Cluster.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Clustering.Objects;
+
+namespace Clustering.Objects
+{
+    class Cluster
+    {
+        private List<CleanObject> objects;
+
+        public List<CleanObject> Get => objects;
+
+        public void Add(CleanObject obj)
+        {
+            objects.Add(obj);
+        }
+    }
+}
