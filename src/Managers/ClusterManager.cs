39be7c29e3dc83cfb5a5abae02d2266e19f10c9f
olga <bachisheo@gmail.com>
2021-11-15 19:15:15 +0700

    added ClusterManager

---
+++ src/Managers/ClusterManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Clustering.Objects;
+
+namespace Clustering.src.Managers
+{
+    class ClusterManager
+    {
+        private List<CleanObject> objects;
+        private List<Cluster> clusters;
+
+    }
+}
