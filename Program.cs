3f63259bd2b44264a8ce7a27425690b6cb45d446
olga <bachisheo@gmail.com>
2021-11-15 18:36:25 +0700

    clear a 'main' branch

--- Program.cs
+++ Program.cs
@@ -23,96 +23,9 @@ namespace Clustering
         }
         static void Main()
         {
-            TestIteratorAndComposite();
+       
         }
 
 
-        static void TestIteratorAndComposite()
-        {
-            double[] rawData = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
-            List<IHierarchyComponent> component = new List<IHierarchyComponent>();
-            foreach (var data in rawData)
-            {
-                double[] nums = { data };
-                component.Add(new HierarchyLeaf(new CleanObject(nums)));
-            }
-
-            List<HierarchyComposite> composite = new List<HierarchyComposite>(6);
-            for (int i = 0; i < composite.Capacity; i++)
-            {
-                composite.Add(new HierarchyComposite());
-            }
-
-            composite[0].SetChildren(composite[1], composite[4]);
-            composite[1].SetChildren(composite[2], composite[3]);
-            composite[2].SetChildren(composite[5], component[0]);
-            for (int i = 3, leafIt = 1; i < composite.Count; i++)
-            {
-                composite[i].SetChildren(component[leafIt++], component[leafIt++]);
-            }
-
-            Console.WriteLine("\nIterate from leaf:");
-            HierarchyIterator it = component[0].CreateIterator();
-            while (it.HasNext())
-            {
-                Console.WriteLine(it.GetNext().Info());
-            }
-
-            Console.WriteLine("\nIterate from root:");
-            it = composite[0].CreateIterator();
-            while (it.HasNext())
-            {
-                Console.WriteLine(it.GetNext().Info());
-            }
-
-
-            Console.WriteLine("\nIterate from node:");
-            it = composite[^1].CreateIterator();
-
-            while (it.HasNext())
-            {
-                Console.WriteLine(it.GetNext().Info());
-            }
-        }
-
-        static void TestTree()
-        {
-            double[] rawData = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
-            List<IHierarchyComponent> comp = new List<IHierarchyComponent>();
-            foreach (var data in rawData)
-            {
-                double[] nums = { data };
-                comp.Add(new HierarchyLeaf(new CleanObject(nums)));
-            }
-
-            List<HierarchyComposite> composites = new List<HierarchyComposite>(6);
-            HierarchyComposite root = new HierarchyComposite();
-            HierarchyComposite left = new HierarchyComposite();
-            HierarchyComposite leftLeft = new HierarchyComposite();
-            HierarchyComposite leftRight = new HierarchyComposite();
-            HierarchyComposite rightLeft = new HierarchyComposite();
-            HierarchyComposite right = new HierarchyComposite();
-            root.SetChildren(left, right);
-            left.SetChildren(leftLeft, leftRight);
-            int leafIt = 0;
-            leftLeft.SetChildren(comp[leafIt++], comp[leafIt++]);
-            leftRight.SetChildren(comp[leafIt++], comp[leafIt++]);
-            rightLeft.SetChildren(comp[leafIt++], comp[leafIt++]);
-            right.SetChildren(rightLeft, comp[leafIt++]);
-
-
-            HierarchyIterator it = root.CreateIterator();
-            while (it.HasNext())
-            {
-                Console.WriteLine(it.GetNext().Info());
-            }
-            comp.Add(root);
-            comp.Add(left);
-            comp.Add(right);
-            foreach (var component in comp)
-            {
-                Console.WriteLine(component.Info());
-            }
-        }
     }
 }
