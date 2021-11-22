dc6c951e4d59195545352d3b471df5ab722c7203
olga <bachisheo@gmail.com>
2021-11-22 22:53:28 +0700

    restore files

---
+++ src/Managers/HierarchyManager.cs
@@ -0,0 +1,22 @@
+﻿using Clustering.Charts;
+using Clustering.Clusterizators;
+using Clustering.Clusterizators.Hierarchy;
+using Clustering.src.Charts;
+using Clustering.src.Managers;
+
+namespace Clustering.Managers
+{
+    public class HierarchyManager : ClusteringManager
+    {
+       
+        protected override IClusterizer CreateClusterizer()
+        {
+            return new HierarchyClusterizer();
+        }
+        protected override IChart CreateChart()
+        {
+            return new TreeChart();
+
+        }
+    }
+}
