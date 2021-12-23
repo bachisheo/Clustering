a8e0ae2745f2deddae939568c6020b6e8d68e197
olga <bachisheo@gmail.com>
2021-12-23 23:54:48 +0700

    logger optimize

--- src/Objects/Logger.cs
+++ src/Objects/Logger.cs
@@ -1,5 +1,7 @@
 ﻿using System.IO;
+using System.Text;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
 namespace Clustering.Objects
 {
@@ -7,18 +9,38 @@ namespace Clustering.Objects
     {
         private static string loggPath = "clust.log";
         private static Logger instance;
-
+        private StreamWriter _sw;
+        private StringBuilder _sb;
+        private static int _sizeToUpdate = 300;
         public static Logger Instance
         {
             get { return instance ??= new Logger(); }
         }
-        private Logger() { }
+
+        private Logger()
+        {
+            _sb = new StringBuilder();
+        }
 
         public void Log(string info)
         {
-            var sw = new StreamWriter(loggPath, true, System.Text.Encoding.UTF8);
-            sw.WriteLine(System.DateTime.Now + " " + info);
-            sw.Close();
+            _sb.AppendLine(System.DateTime.Now + " " + info);
+            if (_sb.Length >= _sizeToUpdate)
+            {
+                Update();
+            }
+        }
+
+        public void Update()
+        {
+            _sw ??= new StreamWriter(loggPath, true, System.Text.Encoding.UTF8);
+            _sw.Write(_sb.ToString());
+            _sb.Clear();
+        }
+        ~Logger()
+        {
+            Update();
+            _sw.Close();
         }
     }
 }
